b1dc1b6dc8c78cbda1f802cc4b2e98401eb5d05e
Coders Key <designer.afride@gmail.com>
2023-11-17 20:59:25 +0600

    sumit 10 class added

--- sumit-redux/react-twcss-rtk-starter - Copy/src/redux/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import transactionsReducer from "./features/transaction/transactionSlice";
-
-export const store = configureStore({
-  reducer: {
-    transactions: transactionsReducer,
-  },
-});
